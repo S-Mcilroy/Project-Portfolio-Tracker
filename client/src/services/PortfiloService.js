d203d1817ba94f78eaeaf8005bffbc5d37393ea0
Steven Mcilroy <stevenmcilroy@msn.com>
2020-02-28 15:40:16 +0000

    helper to query mongoDB

--- client/src/services/PortfiloService.js
+++ client/src/services/PortfiloService.js
@@ -1,5 +1,33 @@
 const baseURL = "http://localhost:3000/api/stocks/"
 
 export default {
-  
+
+  getStocks(){
+    return fetch(baseURL)
+    .then(res => res.json())
+  },
+
+  postStock(payload){
+    return fetch(baseURL, {
+      method: 'POST',
+      body: JSON.stringify(payload),
+      headers: {'Content-Type': 'application/json'}
+    })
+    .then(res => res.json())
+  },
+
+  deleteStock(id){
+    return fetch(baseURL + id, {
+      method: 'DELETE'
+    })
+  },
+
+  updateStock(id, payload){
+    return fetch(baseURL + id, {
+      method: 'PUT',
+      body: JSON.stringify(payload),
+      headers: {'Content-Type': 'application/json'}
+    })
+    .then(res => res.json())
+  }
 }
