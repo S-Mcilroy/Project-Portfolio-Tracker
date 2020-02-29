6c0b3d155e36e4e2092ebc582c4d3ccd6bf665e4
Steven Mcilroy <stevenmcilroy@msn.com>
2020-02-29 15:18:49 +0000

    updated components to use API data

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -6,20 +6,13 @@ db.stocks.insertMany([
   {
     companyName: "Google",
     ticker: "GOOGL",
-    volumeOfStocks: 230,
+    volumeOfStocks: 1,
     purchasePrice: 231.43
   },
   {
     companyName: "Apple",
     ticker: "AAPL",
-    volumeOfStocks: 50,
+    volumeOfStocks: 1,
     purchasePrice: 530.10
-  },
-  {
-    companyName: "JP Morgan",
-    ticker: "JPM-F",
-    volumeOfStocks: 100,
-    purchasePrice: 380.42
   }
-
 ]);
