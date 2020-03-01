a30f8d5968b3474d9a4fbf9cc52981bf8de0da85
User <user@Users-MacBook-Pro.local>
2020-03-01 12:26:47 +0000

    Volume input added, incl input restrictions; volume passes to DB

--- client/src/components/center_panel/ListItem.vue
+++ client/src/components/center_panel/ListItem.vue
@@ -12,8 +12,10 @@
       <li>Exchange: {{stock.profile.exchange}}</li>
     </ul>
 
+    <label for="amount">Volume:</label>
+    <input v-model="amount" type="number" name="amount" value="0" step="1" min="1">
     <button v-on:click="addToPortfolio">Add to Portfolio</button>
-    <!-- <button v-on:click="removeFromPortfolio">Remove from Portfolio</button> -->
+
 <hr>
 
   </div>
@@ -31,17 +33,23 @@ import PortfolioService from '../../services/PortfolioService.js';
 export default {
   name: "list-item",
   props: ['stock'],
-
+  data(){
+    return {
+      amount: null
+    }
+  },
   methods: {
   addToPortfolio(e){
-          e.preventDefault()
-          const game = {
+          e.preventDefault();
+          const newStock = {
             name: this.stock.profile.companyName,
             ticker: this.stock.symbol,
-            volumeOfStocks: 44,
+            volumeOfStocks: parseInt(this.amount),
             purchasePrice: this.stock.profile.price
+          };
+          if  (this.amount > 0 && typeof(parseInt(this.amount)) === 'number') {
+            PortfolioService.postStock(newStock)
           }
-          PortfolioService.postStock(game)
         },
 
   // removeFromPortfolio(){
