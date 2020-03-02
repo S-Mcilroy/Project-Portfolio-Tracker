df601ec224db6d9f4dd6f105ae79b6df650ff158
Steven Mcilroy <stevenmcilroy@msn.com>
2020-03-02 18:44:27 +0000

    fixed {object object} from being default search

--- client/src/components/center_panel/SearchBar.vue
+++ client/src/components/center_panel/SearchBar.vue
@@ -34,7 +34,7 @@ export default {
   name: 'search',
   data () {
     return {
-      selectedStockInput: {},
+      selectedStockInput: null,
       stocksInput: [],
       allSelectedSymbols: [],
       dataItem: [],
